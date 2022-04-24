ef5e35e1ffe5a83d346dbd81b922e23d19b99d7b
ewbdoohyun <ewb.doohyun@gmail.com>
2022-04-24 13:19:34 +0900

    feat : [005-SELLER_SIGN_UP_IN] 셀러에 대한 사인업, 사이인 구현 seller/* 에 대한 token filter 적용

--- user-api/src/main/java/com/zerobase/cms/user/config/FeignConfig.java
+++ user-api/src/main/java/com/zerobase/cms/user/config/FeignConfig.java
@@ -2,15 +2,19 @@ package com.zerobase.cms.user.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
 
+    @Value(value = "${mailgun.key}")
+    private String mailgunKey;
+
     @Qualifier(value = "mailgun")
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
-        return new BasicAuthRequestInterceptor("api","key-4a0dd11ff8793c8de1587d30dde3a8e1");
+        return new BasicAuthRequestInterceptor("api",mailgunKey);
     }
 }
